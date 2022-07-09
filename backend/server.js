65fff13f7de71dbd0635586f0121bd9bdf17dfcb
Rahvayana32 <191240000915@unisnu.ac.id>
2022-07-09 10:30:07 +0700

    Daftar route lain test postman

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,5 @@ const app = express()
 app.get('/api/goals',(req,res)=>{
     res.json({message:'Get Goals'})
 })
+app.use('/api/goals',require('./routes/goalRoutes.js'))
 app.listen(port, () => console.log(`Server started on port ${port}`))
