db114c9da726ddc69cf1f9c4719bc191197d564e
Rahvayana32 <191240000915@unisnu.ac.id>
2022-07-13 22:47:18 +0700

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,41 @@
 // @desc    Register user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req,res) => {
-    res.json({ message: "Register User"})
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+const registerUser = asyncHandler(async(req,res) => {
+    const { name, email, password } = req.body
+    if(!name || !email || !password){
+        res.status(400)
+        throw Error('Please add all fields')
+    }
+//check if user exist
+const userExists = await User.findOne({email})
+
+if(userExists){
+    res.status(400)
+    throw Error('User already exists')
 }
+    res.json({ message: "Register User"})
+})
 
 module.exports = { registerUser } 
 // @desc    Login user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req,res) => {
+const loginUser = asyncHandler(async(req,res) => {
     res.json({ message: "Login User"})
-}
+})
 
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Public
-const getMe = (req,res) => {
+const getMe = asyncHandler(async(req,res) => {
     res.json({ message: "User data display"})
-}
+})
 
 module.exports = { 
     registerUser,
